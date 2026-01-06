d0849823174264e8bc8ce49cb8c8d28318a039e1
Kirankumar Reddy Lingasani <kirankumarreddy7312@gmail.com>
2026-01-06 15:18:25 +0530

    Update coffee_shop.py

--- coffee_shop.py
+++ coffee_shop.py
@@ -1,26 +1,51 @@
-def coffee_shop(name, drink, ):
-    if drink == "coffee":
-        print(f"wow! that's a good choice, {name} \nwait for 5 minuts and take your order")
-        return f"{name}, please take your coffee"        
-    elif drink == "tea":
-        print(f"wow that's a good choice, {name} \nwait for 5 minuts and take your order")
-        return f"{name}, please take your tea"
-    elif drink == "boost":
-        print(f"wow that's a good choice, {name} \nwait for 5 minuts and take your order")
-        return f"{name}, please take your boost"
-    elif drink == "horlicks":
-        print(f"wow that's a good choice, {name} \nwait for 5 minuts and take your order")
-        return f"{name}, please take your horlicks"
-    elif drink == "milk":
-        print(f"wow that's a good choice, {name} \nwait for 5 minuts and take your order")
-        return f"{name}, please take your milk"
-    elif drink == "water":
-        print(f"wow that's a good choice, {name} \nwait for 5 minuts and take your order")
-        return f"{name}, please take your water"
-    elif drink == "juice":
-        print(f"wow that's a good choice, {name} \nwait for 5 minuts and take your order")
-        return f"{name}, please take your juice"
+def coffe_shop():
+    name = input("Please enter your name")
+
+    print(f"Welcome to our coffee shop\n Hi {name} Choose your drink:")
+
+    print("\n1.Coffee\n2.Tea\n3.Lemon Tea\n4.Boost\n5.Milk\n6.Horlicks\n7.Ginger Tea\n8.Water\n9.Cappucino")
+
+    choice = input("Enter your drink number: ")
+    drink = {
+        "1": "Coffee",
+        "2": "Tea",
+        "3": "Lemon Tea",
+        "4": "Boost",
+        "5": "Milk",
+        "6": "Horlicks",
+        "7": "Ginger Tea",
+        "8" : "Water",
+        "9" : "Cappucino"
+    }
+    if choice in drink:
+        quantity = int(input("Enter the quantity"))
+        price = [20, 10, 15, 25, 12, 18, 22, 10, 25]
+        item = price[int(choice) - 1]
+        total = quantity * item 
+        print(f"{name}, you selected {drink[choice]}")
+        if total > 150:
+            discount = total*3/100
+            print(f"Your total is {total} and discount is {discount}")
+            print(f"{name}!Please wait for your order to be ready")
+        elif total >= 150 and total <= 249:
+            discount = total*5/100
+            print(f"Your total is {total} and discount is {discount}")
+            print(f"{name}!Please wait for your order to be ready")
+        elif total > 250 and total <= 400:
+            discount = total*7/100
+            print(f"Your total is {total} and discount is {discount}")
+            print(f"{name}!Please wait for your order to be ready")
+        elif total >= 401 and total <= 600:
+            discount = total*10/100
+            print(f"Your total is {total} and discount is {discount}")
+            print(f"{name}!Please wait for your order to be ready")
+        else:
+            print(f"Your total amount to be paid is {total}\nNote:{name} Add more items and get more discount")
+            print(f"{name}!Please wait for your order to be ready")
+            print("Thanks for visiting our coffee shop")
+            print(f"{name}, Tell us your experience about the our coffee shop")
+            experience = input("Enter your experience: ")
+        print("Thank you for your feedback! have a nice day\n        Visit again          ")
     else:
-        return f"{name}, please enter a valid drink"
-# coffee_shop(input("Enter your name: "), input("Enter your drink (coffee, tea, boost, horlicks, milk, water, juice): "))
-print(f"{coffee_shop(input("Enter your name: "), input("Enter your drink (coffee, tea, boost, horlicks, milk, water, juice): "))}")
+        print(f"{name} your drink is not available, please select from the list")
+coffe_shop()
